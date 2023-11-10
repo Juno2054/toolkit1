db41fdd8ffb5890706bec7584821c46f10e15350
Juno <wnsgh2098@gmail.com>
2023-11-10 17:16:25 +0900

    기본적? 틀 완성?

---
+++ src/components/Detail.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import styled from 'styled-components';
+import { Container } from '../App'
+function Detail(props) {
+
+  return (
+    <Container>
+        아무튼 디테일
+
+    </Container>
+  )
+}
+
+export default Detail
